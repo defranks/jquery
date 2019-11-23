ce81d9933ff1ae94447aa3b5786cb11eb7068c0a
defranks <43969651+defranks@users.noreply.github.com>
2019-11-23 11:18:22 -0500

    Add files via upload

--- js/challenge1.js
+++ js/challenge1.js
@@ -1,18 +1,3 @@
-subscribe.onchange = function() {
-	var x = document.querySelector("#emailField");
-	if (this.checked){
-		console.log("selected");
-    	x.style.display = "block";
-  	} else {
-  		console.log("Not selected");
-    	x.style.display = "none";
-  	}
-  // 	Advanced Option....
-  // 	x.style.display == "block" ?
-		// x.style.display = "none" :
-		// x.style.display = "block";
-}
-
 $("subscribe").click(function(){
 	if (this.checked){
 		var x = $("#emailField");
@@ -22,5 +7,3 @@ $("subscribe").click(function(){
 		}
 	}
 );
-
-// everytime u get rid of document.querySelector swap it w $
